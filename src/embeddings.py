80a3496767ec218e2f1146682c11545d97ffef87
areeshrashid6 <areeshrashid6@gmail.com>
2026-09-04 10:08:56 +0500

    Delete src directory

--- src/embeddings.py
+++
@@ -1,7 +0,0 @@
-"""Thin wrapper around the embeddings model so it's configured in one place."""
-
-from langchain_openai import OpenAIEmbeddings
-
-
-def get_embeddings(model: str = "text-embedding-3-small") -> OpenAIEmbeddings:
-    return OpenAIEmbeddings(model=model)
